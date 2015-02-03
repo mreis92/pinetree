b46869560788c21f7af4a470a5a2b10f8deea1ce
Manuel Reis <manuel.reis@ist.utl.pt>
2015-02-03 14:53:05 +0000

    Added some includes. Moved constants to constants.h.

--- annotation.h
+++ annotation.h
@@ -3,10 +3,10 @@
 
 #include <stdio.h>
 
+#include "constants.h"
 #include "dataset.h"
+#include "strmap.h"
 #include "util.h"
-
-#define NUM_SIZE 7
 
 void annotate_targets(dataset_t *tds, char* annotation_file);
 
