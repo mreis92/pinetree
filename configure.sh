1c03a3b315446d2c322631d3490b1fc9a53e1cb0
Manuel Reis <manuel.reis@ist.utl.pt>
2015-02-03 14:53:50 +0000

    Including latest version of ViennaPackage and new make command.

--- configure.sh
+++ configure.sh
@@ -1,7 +1,6 @@
-#wget http://www.tbi.univie.ac.at/RNA/download.php?id=viennarna-2.1.8 -O ViennaRNA.tar.gz
-#tar -zxvf ViennaRNA.tar.gz
-#rm ViennaRNA.tar.gz
+wget http://www.tbi.univie.ac.at/RNA/download.php?id=viennarna-2.1.8 -O ViennaRNA.tar.gz
+tar -zxvf ViennaRNA.tar.gz
+rm ViennaRNA.tar.gz
 
-#(cd ViennaRNA-2.1.8; ./configure; make)
-make 
-make evo 
+(cd ViennaRNA-2.1.8; ./configure; make)
+make all
