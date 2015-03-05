be444a0f96ce600dd91ac329f87d73864330f3a2
Manuel Reis <manuel.reis@ist.utl.pt>
2015-03-05 15:40:35 +0000

    New evalue variable.

--- constants.h
+++ constants.h
@@ -1,21 +1,23 @@
 #ifndef CONSTANTS_H
 #define CONSTANTS_H
 
+#define VERSION 1.0
+
 /*Program execution constants */
-#define NUM_PROCESSORS 1
+#define NUM_PROCESSORS 20
 #define GAP -1
 #define MATCH 1
 #define MISMATCH -1
 #define GU -0.5
-#define SEED_PENALTY -0.5
 #define SEED_START 2 
 #define SEED_STOP 12
 #define CR_START 9
 #define CR_STOP 11
 #define NUM_ERRORS 1
 #define MARKOV_ORDER 6 
+#define EVALUE 150
 #define C_THRESHOLD 3.5
-#define A_THRESHOLD 16
+#define A_THRESHOLD 15.5
 #define E_THRESHOLD 7
 
 /* If you want to allow N errors in your sequences, define NUM_ERRORS as N+1 */
